00280ccb67dc2771fc32a45cd9817ed040da6c08
marroccuandrea <marroccuandrea@yahoo.it>
2024-06-18 19:02:58 +0200

    Card ristoranti

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -7,7 +7,120 @@ export default {
 </script>
 
 <template>
-  <div></div>
+  <div class="container">
+    <div class="row row-cols-3">
+      <div class="col">
+        <div class="card">
+          <img src="../../../public/test.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Ristorante</h5>
+            <a href="#" class="btn px-4">Scopri</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../../../public/test.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Ristorante</h5>
+            <a href="#" class="btn px-4">Scopri</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../../../public/test.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Ristorante</h5>
+            <a href="#" class="btn px-4">Scopri</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../../../public/test.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Ristorante</h5>
+            <a href="#" class="btn px-4">Scopri</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../../../public/test.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Ristorante</h5>
+            <a href="#" class="btn px-4">Scopri</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../../../public/test.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Ristorante</h5>
+            <a href="#" class="btn px-4">Scopri</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../../../public/test.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Ristorante</h5>
+            <a href="#" class="btn px-4">Scopri</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../../../public/test.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Ristorante</h5>
+            <a href="#" class="btn px-4">Scopri</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="../../../public/test.png" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Ristorante</h5>
+            <a href="#" class="btn px-4">Scopri</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  margin-bottom: 20px;
+  height: 20rem;
+  background-color: #f6b802;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5%;
+  border-radius: 15px;
+  border: none;
+  img {
+    width: 80%;
+  }
+  .card-title {
+    color: white;
+  }
+  .card-body {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    .btn {
+      background-color: #2dc4b6;
+      color: white;
+      border-radius: 15px;
+      border: none;
+    }
+  }
+}
+</style>
