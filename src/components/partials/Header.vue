f16f2ba4830fd4de155dbcec3dae7844eb017998
marroccuandrea <marroccuandrea@yahoo.it>
2024-06-18 14:45:47 +0200

    Header

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -4,36 +4,36 @@ export default {};
 
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-          </ul>
-          <form class="form-inline">
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <button class="input-group-text" id="basic-addon1">
-                  Cerca
-                </button>
-              </div>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Cerca il ristorante"
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-              />
-            </div>
-          </form>
-        </div>
+    <div class="d-flex w-100 justify-content-between">
+      <div class="sx d-flex align-items-center">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Accedi</a>
+          </li>
+        </ul>
       </div>
-    </nav>
+      <div class="dx d-flex align-items-center me-3">
+        <form class="d-flex" role="search">
+          <button class="btn btn-warning" type="submit">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+          <input
+            class="form-control"
+            type="search"
+            placeholder="Cerca"
+            aria-label="Search"
+          />
+        </form>
+        <ul>
+          <li>
+            <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </header>
 </template>
 
@@ -44,10 +44,30 @@ header {
   padding: 0 !important;
   display: flex;
   align-items: center;
+  .btn {
+    border-radius: 5px 0 0 5px;
+  }
+  .form-control {
+    border-radius: 0 5px 5px 0;
+  }
 
-  .container-fluid {
+  ul {
+    margin-bottom: 0;
     display: flex;
-    justify-content: space-between;
+    li {
+      padding: 0 15px;
+      list-style: none;
+      a {
+        color: white;
+        text-decoration: none;
+        &:hover {
+          color: orange;
+        }
+        &.active {
+          color: yellow;
+        }
+      }
+    }
   }
 }
 </style>
