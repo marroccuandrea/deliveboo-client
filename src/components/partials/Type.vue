d89505d21dbba50a07c6f7ef61c807503fb23168
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-06-19 11:20:50 +0200

    refactor: Update Type.vue and TypeCard.vue styling

--- src/components/partials/Type.vue
+++ src/components/partials/Type.vue
@@ -1,49 +1,51 @@
 <template>
-  <div class="swiper-bg desktop">
-    <swiper-container
-      :loop="true"
-      :speed="2000"
-      direction="vertical"
-      :slides-per-view="7"
-      :space-between="10"
-      class="swiper-desktop"
-    >
-      <swiper-slide><a href="#">Italiano</a></swiper-slide>
-      <swiper-slide><a href="#">Cinese</a></swiper-slide>
-      <swiper-slide><a href="#">Giapponese</a></swiper-slide>
-      <swiper-slide><a href="#">Messicano</a></swiper-slide>
-      <swiper-slide><a href="#">Vietnamita</a></swiper-slide>
-      <swiper-slide><a href="#">Indiano</a></swiper-slide>
-      <swiper-slide><a href="#">Vegano</a></swiper-slide>
-      <swiper-slide><a href="#">Pizza</a></swiper-slide>
-      <swiper-slide><a href="#">Coreano</a></swiper-slide>
-      <swiper-slide><a href="#">Spagnolo</a></swiper-slide>
-      <swiper-slide><a href="#">Greco</a></swiper-slide>
-      <swiper-slide><a href="#">Vegetariano</a></swiper-slide>
-    </swiper-container>
-  </div>
-  <div class="swiper-bg-2 tablet">
-    <swiper-container
-      :loop="true"
-      :speed="2000"
-      direction="horizontal"
-      :slides-per-view="3"
-      :space-between="10"
-      class="swiper-tablet"
-    >
-      <swiper-slide><a href="#">Italiano</a></swiper-slide>
-      <swiper-slide><a href="#">Cinese</a></swiper-slide>
-      <swiper-slide><a href="#">Giapponese</a></swiper-slide>
-      <swiper-slide><a href="#">Messicano</a></swiper-slide>
-      <swiper-slide><a href="#">Vietnamita</a></swiper-slide>
-      <swiper-slide><a href="#">Indiano</a></swiper-slide>
-      <swiper-slide><a href="#">Vegano</a></swiper-slide>
-      <swiper-slide><a href="#">Pizza</a></swiper-slide>
-      <swiper-slide><a href="#">Coreano</a></swiper-slide>
-      <swiper-slide><a href="#">Spagnolo</a></swiper-slide>
-      <swiper-slide><a href="#">Greco</a></swiper-slide>
-      <swiper-slide><a href="#">Vegetariano</a></swiper-slide>
-    </swiper-container>
+  <div>
+    <div class="swiper-bg desktop">
+      <swiper-container
+        :loop="true"
+        :speed="2000"
+        direction="vertical"
+        :slides-per-view="7"
+        :space-between="10"
+        class="swiper-desktop"
+      >
+        <swiper-slide><a href="#">Italiano</a></swiper-slide>
+        <swiper-slide><a href="#">Cinese</a></swiper-slide>
+        <swiper-slide><a href="#">Giapponese</a></swiper-slide>
+        <swiper-slide><a href="#">Messicano</a></swiper-slide>
+        <swiper-slide><a href="#">Vietnamita</a></swiper-slide>
+        <swiper-slide><a href="#">Indiano</a></swiper-slide>
+        <swiper-slide><a href="#">Vegano</a></swiper-slide>
+        <swiper-slide><a href="#">Pizza</a></swiper-slide>
+        <swiper-slide><a href="#">Coreano</a></swiper-slide>
+        <swiper-slide><a href="#">Spagnolo</a></swiper-slide>
+        <swiper-slide><a href="#">Greco</a></swiper-slide>
+        <swiper-slide><a href="#">Vegetariano</a></swiper-slide>
+      </swiper-container>
+    </div>
+    <div class="swiper-bg-2 tablet">
+      <swiper-container
+        :loop="true"
+        :speed="2000"
+        direction="horizontal"
+        :slides-per-view="3"
+        :space-between="10"
+        class="swiper-tablet"
+      >
+        <swiper-slide><a href="#">Italiano</a></swiper-slide>
+        <swiper-slide><a href="#">Cinese</a></swiper-slide>
+        <swiper-slide><a href="#">Giapponese</a></swiper-slide>
+        <swiper-slide><a href="#">Messicano</a></swiper-slide>
+        <swiper-slide><a href="#">Vietnamita</a></swiper-slide>
+        <swiper-slide><a href="#">Indiano</a></swiper-slide>
+        <swiper-slide><a href="#">Vegano</a></swiper-slide>
+        <swiper-slide><a href="#">Pizza</a></swiper-slide>
+        <swiper-slide><a href="#">Coreano</a></swiper-slide>
+        <swiper-slide><a href="#">Spagnolo</a></swiper-slide>
+        <swiper-slide><a href="#">Greco</a></swiper-slide>
+        <swiper-slide><a href="#">Vegetariano</a></swiper-slide>
+      </swiper-container>
+    </div>
   </div>
 </template>
 
@@ -59,8 +61,7 @@ export default {
 </script>
 
 <style scoped>
-
-.swiper-bg{
+.swiper-bg {
   background-color: #26a69a;
   height: 1000px;
   display: flex;
@@ -69,7 +70,8 @@ export default {
   padding: 10% 10%;
   border-radius: 50px;
 }
-.swiper-bg-2{
+
+.swiper-bg-2 {
   background-color: #26a69a;
   height: 500px;
   display: flex;
@@ -85,56 +87,42 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-
-  swiper-slide {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #f9a825; 
-    color: white;
-    font-size: 20px;
-    border-radius: 20px;
-    padding: 10% !important;
-    a {
-      color: white;
-      text-decoration: none;
-      font-size: 40px;
-    }
-  }
 }
 
-.swiper-tabet {
-  height: 90%;
+.swiper-tablet {
+  height: auto;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-
-  swiper-slide {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #f9a825; 
-    color: white;
-    font-size: 20px;
-    border-radius: 20px;
-    padding: 10% !important;
-    a {
-      color: white;
-      text-decoration: none;
-      font-size: 40px;
-    }
-  }
 }
+
+swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f9a825; 
+  color: white;
+  font-size: 20px;
+  border-radius: 20px;
+  padding: 10% !important;
+}
+
+swiper-slide a {
+  color: white;
+  text-decoration: none;
+  font-size: 40px;
+}
+
 @media (min-width: 769px) {
   .tablet {
     display: none;
   }
 }
+
 @media (max-width: 768px) {
   .desktop {
     display: none;
   }
 }
-
 </style>
