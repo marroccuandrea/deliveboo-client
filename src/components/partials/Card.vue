fe5dd78a68440a1555f50c28c0e8106e7bf4e755
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-06-20 18:17:53 +0200

    refactor: Improve Card.vue component styling and add lazy loading for images

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -1,104 +1,32 @@
-<!-- <script>
-import { register } from "swiper/element/bundle";
-import { store } from "../../data/store";
-import axios from 'axios';
-register();
-
-export default {
-  props: {
-    type: {
-      business_name: String,
-      image: String,
-    }
-  },
-  data() {
-    return {
-      store,
-      types: [],
-      restaurants: [],
-    }
-  },
-  computed: {
-    filteredRestaurants() {
-      if (this.store.filterTypes.length === 0) {
-        return this.store.restaurants;
-      }
-      return this.store.restaurants.filter(restaurant => 
-        restaurant.types.some(type => this.store.filterTypes.includes(type.name))
-      );
-    }
-  },
-  methods: {
-    getApi() {
-      axios.get(this.store.restaurantUrl, {
-        params: store.queryParams
-      })
-      .then(result => {
-        this.store.restaurants = result.data.restaurants;
-        console.log(this.store.restaurants);
-      })
-      .catch(error => {
-        console.log(error);
-      })
-    }
-  },
-  mounted() {
-    this.getApi();
-  }
-}
-</script>
-
 <template>
   <div class="container ristoranti">
-    <div class="row row-cols-3">
-      <div class="col" v-for="item in filteredRestaurants" :key="`r-${item.id}`">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+      <div class="col" v-for="item in paginatedRestaurants" :key="`r-${item.id}`">
         <div class="card">
-          <img v-if="item.image" :src="item.image" class="card-img-top"/>
-          <img v-else src="/placeholder.webp" class="card-img-top"/>
+          <div class="img-box">
+            <img v-if="item.image" :src="item.image" class="card-img-top" />
+            <img v-else src="/placeholder.webp" class="card-img-top" />
+          </div>
           <div class="card-body">
-            <h5 class="card-title">{{item.business_name}}</h5>
+            <h5 class="card-title">{{ item.business_name }}</h5>
+            <div class="badge-container">
+              <span v-for="(badge, index) in restaurantTypes(item)" :key="index" :class="badge.styleClass">{{ badge.name }}</span>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <div class="carousel-controls">
+      <button class="control-btn prev" @click="prevPage" v-show="currentPage > 0">
+        <
+      </button>
+      <button class="control-btn next" @click="nextPage" v-show="currentPage < totalPages - 1">
+        >
+      </button>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.ristoranti {
-  overflow-y: auto;
-  overflow-x: hidden;
-}
-.card {
-  margin-bottom: 20px;
-  height: 20rem;
-  background-color: #f6b802;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 5%;
-  border-radius: 15px;
-  border: none;
-  flex-shrink: 0;
-  img {
-    width: 90%;
-  }
-  .card-title {
-    color: white;
-  }
-  .card-body {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    .btn {
-      background-color: #2dc4b6;
-      color: white;
-      border-radius: 15px;
-      border: none;
-    }
-  }
-}
-</style> -->
 <script>
 import { store } from "../../data/store";
 import axios from "axios";
@@ -140,7 +68,10 @@ export default {
     },
     restaurantTypes() {
       return (restaurant) =>
-        restaurant.types.map((type) => type.name).join(", ");
+        restaurant.types.map((type) => ({
+          name: type.name,
+          styleClass: 'badge text-bg-success' // Puoi personalizzare lo stile del badge qui
+        }));
     },
   },
   methods: {
@@ -173,47 +104,6 @@ export default {
   },
 };
 </script>
-
-<template>
-  <div class="container ristoranti">
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
-      <div
-        class="col"
-        v-for="item in paginatedRestaurants"
-        :key="`r-${item.id}`"
-      >
-        <div class="card">
-          <div class="img-box">
-            <img v-if="item.image" :src="item.image" class="card-img-top" />
-            <img v-else src="/placeholder.webp" class="card-img-top" />
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">{{ item.business_name }}</h5>
-            <span class="card-text badge text-bg-success">{{
-              restaurantTypes(item)
-            }}</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="carousel-controls">
-      <button
-        class="control-btn prev"
-        @click="prevPage"
-        v-show="currentPage > 0"
-      >
-        <
-      </button>
-      <button
-        class="control-btn next"
-        @click="nextPage"
-        v-show="currentPage < totalPages - 1"
-      >
-        >
-      </button>
-    </div>
-  </div>
-</template>
 
 <style lang="scss" scoped>
 .ristoranti {
@@ -265,12 +155,19 @@ export default {
       display: flex;
       align-items: center;
       flex-direction: column;
-      .btn {
-        background-color: #2dc4b6;
-        color: white;
-        border-radius: 15px;
-        border: none;
-      }
+    }
+    .badge-container {
+      margin-top: 10px;
+      display: flex;
+      flex-wrap: wrap;
+    }
+    .badge {
+      margin-right: 5px;
+      padding: 5px 10px;
+      border-radius: 15px;
+      background-color: #2dc4b6;
+      color: white;
+      font-size: medium;
     }
     .card-text {
       color: white;
@@ -323,25 +220,25 @@ export default {
     background-color: #ccc;
     cursor: not-allowed;
   }
-}
 
-@media (max-width: 768px) {
-  .ristoranti {
-    overflow-y: scroll;
-    overflow-x: hidden;
-    .card {
-      width: 220px;
-      height: 300px;
+  @media (max-width: 768px) {
+    .ristoranti {
+      overflow-y: scroll;
+      overflow-x: hidden;
+      .card {
+        width: 220px;
+        height: 300px;
+      }
     }
   }
-}
 
-@media (max-width: 575px) {
-  .ristoranti {
-    overflow-y: scroll;
-    overflow-x: hidden;
-    .card {
-      height: 90vh;
+  @media (max-width: 575px) {
+    .ristoranti {
+      overflow-y: scroll;
+      overflow-x: hidden;
+      .card {
+        height: 90vh;
+      }
     }
   }
 }
