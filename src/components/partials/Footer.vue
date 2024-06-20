b4822ef8c6adbbe1ef2036c1352eb9bfae26e59b
marroccuandrea <marroccuandrea@yahoo.it>
2024-06-20 17:34:51 +0200

    link footer

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -6,34 +6,38 @@ export default {};
   <div class="container-fluid footer-container">
     <footer class="text-center text-lg-start" style="background-color: #26a69a">
       <div class="container d-flex justify-content-center py-5">
-        <button
+        <a
+          href="https://www.facebook.com/"
           type="button"
           class="btn btn-primary btn-lg btn-floating mx-2"
           style="background-color: #f9a825"
         >
           <i class="fab fa-facebook-f"></i>
-        </button>
-        <button
+        </a>
+        <a
+          href="https://www.youtube.com/"
           type="button"
           class="btn btn-primary btn-lg btn-floating mx-2"
           style="background-color: #f9a825"
         >
           <i class="fab fa-youtube"></i>
-        </button>
-        <button
+        </a>
+        <a
+          href="https://www.instagram.com/"
           type="button"
           class="btn btn-primary btn-lg btn-floating mx-2"
           style="background-color: #f9a825"
         >
           <i class="fab fa-instagram"></i>
-        </button>
-        <button
+        </a>
+        <a
+          href="https://x.com/?lang=it"
           type="button"
           class="btn btn-primary btn-lg btn-floating mx-2"
           style="background-color: #f9a825"
         >
           <i class="fab fa-twitter"></i>
-        </button>
+        </a>
       </div>
 
       <!-- Copyright -->
