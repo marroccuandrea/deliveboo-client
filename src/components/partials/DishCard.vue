9a67ec1ec56773a6f39130db92e07e0f0e3b5c01
luigidibella <lykos.omega@gmail.com>
2024-06-21 16:44:05 +0200

    [add] cart placeholder

--- src/components/partials/DishCard.vue
+++ src/components/partials/DishCard.vue
@@ -3,6 +3,12 @@ export default {
   props: {
     dishObject: Object,
   },
+
+  methods: {
+    addToCart() {
+      this.$emit('callFunction');
+    }
+  }
 };
 </script>
 
@@ -10,6 +16,8 @@ export default {
   <div class="col">
     <div class="card">
       <h2>{{ dishObject.name }}</h2>
+      <p>{{ dishObject.price }}</p>
+      <button class="btn btn-danger" @click="addToCart">+</button>
     </div>
   </div>
 </template>
