f1c59755b2128fa9d7076e2fe49dcaa86244dd35
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-06-24 15:19:31 +0200

    [add] style in cart

---
+++ src/pages/Checkout.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "Checkout",
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<template>Checkout</template>
+
+<style lang="scss" scoped></style>
