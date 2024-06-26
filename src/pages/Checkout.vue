cfd29cdefe5d1aad139cfa54d791d00804216521
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-06-26 12:00:16 +0200

    [add, to fix] validation user dertails

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -21,7 +21,7 @@ export default {
       paymentFormEnabled: false,
 
       total_order: 0,
-
+      isValid: true,
       errors: {
         name: [],
         surname: [],
@@ -33,6 +33,7 @@ export default {
 
   methods: {
     sendFormData() {
+
       const formData = {
         name: this.name,
         surname: this.surname,
@@ -93,13 +94,134 @@ export default {
         }
       );
     },
+
+    // validateForm(){
+    //   const btnPay = document.getElementById('btn-pay');
+    //   // nome
+    //   const name = document.getElementById('name');
+    //   const errorName = document.getElementById('error-name');
+    //   // cognome
+    //   const surname = document.getElementById('surname');
+    //   const errorSurname = document.getElementById('error-surname');
+    //   // email
+    //   const email = document.getElementById('email');
+    //   const errorEmail = document.getElementById('error-email');
+    //   // telefono
+    //   const phoneNumber = document.getElementById('phone_number');
+    //   const errorPhoneNumber = document.getElementById('error-phone_number');
+    //   // indirizzo
+    //   const address = document.getElementById('address');
+    //   const errorAddress = document.getElementById('error-address');
+
+    //   btnPay.addEventListener('click', function(event){
+    //     event.preventDefault();
+    //     console.log('prova');
+    //     errorName.innerHTML = ''; 
+    //     name.classList.remove('is-invalid');
+    //     errorSurname.innerHTML = ''; 
+    //     surname.classList.remove('is-invalid');
+    //     errorEmail.innerHTML = ''; 
+    //     email.classList.remove('is-invalid');
+    //     errorPhoneNumber.innerHTML = ''; 
+    //     phoneNumber.classList.remove('is-invalid');
+    //     errorAddress.innerHTML = ''; 
+    //     address.classList.remove('is-invalid');
+
+    //     // controlli nome
+    //     if (name.value.trim().length === 0) {
+    //         errorName.innerHTML = 'Il campo Nome è obbligatorio';
+    //         name.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     } else if (name.value.trim().length < 4) {
+    //         errorName.innerHTML = 'Il campo Nome Utente deve avere almeno 4 caratteri';
+    //         name.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     } else if (name.value.trim().length > 50) {
+    //         errorName.innerHTML = 'Il campo Nome Utente non deve avere più di 50 caratteri';
+    //         name.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     }
+
+    //     // controlli cognome
+    //     if (surname.value.trim().length === 0) {
+    //         errorSurname.innerHTML = 'Il campo Cognome è obbligatorio';
+    //         surname.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     } else if (surname.value.trim().length < 4) {
+    //         errorSurname.innerHTML = 'Il campo Cognome deve avere almeno 4 caratteri';
+    //         surname.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     } else if (surname.value.trim().length > 50) {
+    //         errorSurname.innerHTML = 'Il campo Cognome non deve avere più di 50 caratteri';
+    //         surname.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     }
+
+    //     // controlli email
+    //     let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    //     if (email.value.trim().length === 0) {
+    //         errorEmail.innerHTML = 'Il campo Email è obbligatorio';
+    //         email.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     } else if (!emailPattern.test(email.value)) {
+    //         errorEmail.innerHTML = 'Inserisci un indirizzo email valido';
+    //         email.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     } else if (email.value.trim().length > 50) {
+    //         errorEmail.innerHTML = 'Il campo Email non deve essere più lungo di 50 caratteri';
+    //         email.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     }
+
+    //     // controlli telefono
+    //     if (phoneNumber.value.trim().length === 0) {
+    //         errorPhoneNumber.innerHTML = 'Il campo Telefono è obbligatorio';
+    //         phoneNumber.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     } else if (!/^\d+$/.test(phoneNumber.value)) {
+    //         errorPhoneNumber.innerHTML = 'Il campo Telefono può contenere solo numeri';
+    //         phoneNumber.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     } else if(phoneNumber.value.trim().length < 11 || phoneNumber.value.trim().length > 11){
+    //       errorPhoneNumber.innerHTML = 'Il campo Telefono deve avere 11 numeri';
+    //       phoneNumber.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     }
+        
+        
+            
+    //     // controlli indirizzo
+    //     if (address.value.trim().length === 0) {
+    //         errorAddress.innerHTML = 'Il campo Indirizzo è obbligatorio';
+    //         address.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     } else if (address.value.trim().length < 8) {
+    //         errorAddress.innerHTML = 'Il campo Indirizzo deve avere almeno 8 caratteri';
+    //         address.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     } else if (address.value.trim().length > 100) {
+    //         errorAddress.innerHTML = 'Il campo Indirizzo non deve avere più di 100 caratteri';
+    //         address.classList.add('is-invalid');
+    //         this.isValid = false;
+    //     }
+            
+    //   });
+    // },
+
+    
+    
   },
   mounted() {
+    // this.validateForm();
     this.formData = {};
     this.paymentFormEnabled = false;
     this.getClientToken();
   },
 };
+
+
+
 </script>
 
 <template>
@@ -111,48 +233,53 @@ export default {
           <div class="col-md-6">
             <label for="name" class="form-label">Nome</label>
             <input type="text" class="form-control" id="name" v-model="name" />
+            <small id="error-name" class="text-danger fw-semibold"></small>
           </div>
-
+          
           <div class="col-md-6">
             <label for="surname" class="form-label">Cognome</label>
             <input
-              type="text"
-              class="form-control"
-              id="surname"
-              v-model="surname"
+            type="text"
+            class="form-control"
+            id="surname"
+            v-model="surname"
             />
+            <small id="error-surname" class="text-danger fw-semibold"></small>
           </div>
-
+          
           <div class="col-md-6">
             <label for="email" class="form-label">Email</label>
             <input
-              type="email"
-              class="form-control"
+            type="email"
+            class="form-control"
               id="email"
               v-model="email"
-            />
-          </div>
-
-          <div class="col-md-6">
-            <label for="phone_number" class="form-label"
+              />
+              <small id="error-email" class="text-danger fw-semibold"></small>
+            </div>
+            
+            <div class="col-md-6">
+              <label for="phone_number" class="form-label"
               >Numero di telefono</label
-            >
-            <input
+              >
+              <input
               type="text"
               class="form-control"
               id="phone_number"
               v-model="phone_number"
-            />
-          </div>
-
-          <div class="col-12">
-            <label for="address" class="form-label">Indirizzo</label>
+              />
+              <small id="error-phone_number" class="text-danger fw-semibold"></small>
+            </div>
+            
+            <div class="col-12">
+            <label for="c" class="form-label">Indirizzo</label>
             <input
               type="text"
               class="form-control"
               id="address"
               v-model="address"
-            />
+              />
+              <small id="error-address" class="text-danger fw-semibold"></small>
           </div>
           <div class="col-12">
             <label for="note" class="form-label"
@@ -167,7 +294,7 @@ export default {
           </div>
 
           <div class="col-12">
-            <button type="submit" class="btn btn-custom-primary w-auto">
+            <button type="submit" id="btn-pay" class="btn btn-custom-primary w-auto">
               Vai al pagamento
             </button>
           </div>
@@ -198,4 +325,6 @@ export default {
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
