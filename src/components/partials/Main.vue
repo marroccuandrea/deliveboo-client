e52ab6c545706ac12ede9c9243813d257c996a8b
LuigiDiBella <lykos.omega@gmail.com>
2024-06-28 13:16:57 +0200

    [add] paymentSuccessFunction

--- src/components/partials/Main.vue
+++ src/components/partials/Main.vue
@@ -1,13 +1,34 @@
 <script>
+import {store } from '../../data/store';
 import Hero from "./Hero.vue";
 import Swiper from "./Swiper.vue";
 import TypeCard from "./TypeCard.vue";
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
   components: {
     Hero,
     Swiper,
     TypeCard,
   },
+  mounted() {
+    // Controlla se nella URL è presente il parametro payment=success
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('payment') === 'success') {
+      this.paymentSuccessFunction();
+    }
+  },
+  methods: {
+    paymentSuccessFunction() {
+      this.store.cart = [];
+      this.store.restaurantId = null;
+      localStorage.removeItem("cart");
+      localStorage.removeItem("restaurantId");
+    }
+  }
 };
 </script>
 
@@ -21,8 +42,8 @@ export default {
 
 <style lang="scss" scoped>
 @media (max-width: 576px) {
-  main{
+  /* main{
     
-  }
+  } */
 }
 </style>
